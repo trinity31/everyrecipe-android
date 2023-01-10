62893278f01914f1a1a63113f6022928aa4a08f8
trinity31 <kyeongyeon.song@gmail.com>
2023-01-10 16:02:41 +0900

    Setup Bottom Navigation

--- app/src/main/java/com/example/everyrecipe/MainActivity.kt
+++ app/src/main/java/com/example/everyrecipe/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.everyrecipe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.amplifyframework.api.ApiException
 import com.amplifyframework.api.graphql.GraphQLRequest
 import com.amplifyframework.api.graphql.PaginatedResult
@@ -12,11 +15,19 @@ import com.amplifyframework.api.graphql.model.ModelQuery
 import com.amplifyframework.datastore.generated.model.Recipe
 import com.amplifyframework.kotlin.core.Amplify
 import com.example.everyrecipe.data.model.Food
+import com.example.everyrecipe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 
     private suspend fun getRecipe(id: String) {
