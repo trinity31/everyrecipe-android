d8e1a674f5f6e9989e995c9b9b4fae6653b0d7a5
trinity31 <kyeongyeon.song@gmail.com>
2023-01-22 14:55:35 +0900

    Show bookmarked recipes in Bookmark Tab

--- app/src/main/java/com/example/everyrecipe/presentation/home/BookmarkFragment.kt
+++ app/src/main/java/com/example/everyrecipe/presentation/home/BookmarkFragment.kt
@@ -1,60 +1,82 @@
 package com.example.everyrecipe.presentation.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.everyrecipe.R
+import com.example.everyrecipe.data.util.Resource
+import com.example.everyrecipe.databinding.FragmentBookmarkBinding
+import com.example.everyrecipe.presentation.adapters.RecipeListAdapter
+import com.example.everyrecipe.presentation.viewmodel.BookmarkViewModel
+import com.example.everyrecipe.presentation.viewmodel.BookmarkViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BookmarkFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class BookmarkFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val TAG = BookmarkFragment::class.java.simpleName
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    @Inject
+    lateinit var bookmarkFactory: BookmarkViewModelFactory
+    lateinit var bookmarkViewModel: BookmarkViewModel
+
+    private var _binding: FragmentBookmarkBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var recipeListAdapter: RecipeListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bookmark, container, false)
+        _binding = FragmentBookmarkBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BookmarkFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BookmarkFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bookmarkViewModel = ViewModelProvider(this, bookmarkFactory)
+            .get(BookmarkViewModel::class.java)
+
+        initView()
+        initData()
+        initObserve()
+    }
+
+    private fun initView() {
+        recipeListAdapter = RecipeListAdapter(listOf(), viewModel = bookmarkViewModel)
+        binding.bookmarkRv.apply {
+            adapter = recipeListAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun initData() {
+        bookmarkViewModel.getBookmarkedItems()
+    }
+
+    private fun initObserve() {
+        bookmarkViewModel.bookmarks.observe(viewLifecycleOwner) {
+            when(it) {
+                is Resource.Success -> {
+                    Log.i(TAG, "Fetched Bookmarks.")
+                    it.data?.let { recipes ->
+                        recipeListAdapter.recipes = recipes
+                        recipeListAdapter.notifyDataSetChanged()
+                    }
                 }
+                else -> {}
             }
+        }
+
+        bookmarkViewModel.bookmark_result.observe(viewLifecycleOwner) {
+            bookmarkViewModel.getBookmarkedItems()
+        }
     }
 }
